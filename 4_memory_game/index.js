ee9e2c1d01e509a7b6eacf76905ea0e5f643d789
Abdalluh28 <abdok7496@gmail.com>
2025-12-02 16:14:48 +0200

    4-memory game

---
+++ 4_memory_game/index.js
@@ -0,0 +1,50 @@
+const srcs = [
+    './assets/1.gif', './assets/2.gif', './assets/3.gif', './assets/1.gif',
+    './assets/5.gif', './assets/2.gif', './assets/6.gif', './assets/4.gif',
+    './assets/3.gif', './assets/5.gif', './assets/6.gif', './assets/4.gif',
+];
+let moonImage = './assets/moon.gif';
+
+let images = document.querySelectorAll('.memory-game-image')
+
+let isBusy = false;
+let flipped = [];
+let handleImageClick = (image, index) => {
+    if (isBusy) return; // to avoid quick double click
+
+    if (image.classList.contains('matched')) return // to avoid already matched images
+    if (flipped.includes(index)) return; // to avoid double click on the same image
+
+    image.src = srcs[index];
+    flipped.push(index);
+
+    if (flipped.length == 2) {
+        compareFlipped();
+    }
+
+}
+let compareFlipped = () => {
+    let [first, second] = flipped;
+    let match = srcs[first] === srcs[second];
+
+    if (match) {
+        // found a match
+        images[first].classList.add('matched');
+        images[second].classList.add('matched');
+        flipped = [];
+    } else {
+        // not a match, flip them back
+        isBusy = true;
+
+        setTimeout(() => {
+            images[first].src = moonImage;
+            images[second].src = moonImage;
+            isBusy = false;
+            flipped = [];
+        }, 1000);
+    }
+}
+
+images.forEach((image, index) => {
+    image.addEventListener("click", () => handleImageClick(image, index));
+})
